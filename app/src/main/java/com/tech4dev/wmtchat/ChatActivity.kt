af643f8f90ac926394a23759e55b87e638f3f681
AntoniNeutron <antonineutron@gmail.com>
2022-09-06 14:10:16 +0100

    September 6th class - completed the message sending section

--- app/src/main/java/com/tech4dev/wmtchat/ChatActivity.kt
+++ app/src/main/java/com/tech4dev/wmtchat/ChatActivity.kt
@@ -3,16 +3,27 @@ package com.tech4dev.wmtchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
+    private lateinit var messageRV: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        setupToolbar()
+
+        messageRV = findViewById(R.id.messageSection)
+        messageRV.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Username"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
